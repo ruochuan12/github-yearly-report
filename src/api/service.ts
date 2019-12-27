7512aafbb16f61bd0de559650a0af3336f1a96bb
axuebin <xbmail2013@163.com>
2019-12-27 09:52:02 +0800

    feat: restful api 走通

--- src/api/service.ts
+++ src/api/service.ts
@@ -9,6 +9,21 @@ export const fetchGithubTokenService = async (
 };
 
 /**
+ * 获取 userInfo
+ * @param octokit 实例对象
+ * @param payload 参数
+ */
+export const fetchUserInfoService = async (
+  octokit: any,
+  payload?: {},
+): Promise<any> => {
+  const { users } = octokit;
+  const response = await users.getAuthenticated(payload);
+  return response;
+};
+
+
+/**
  * 获取 issues
  * @param octokit 实例对象
  * @param payload 参数
@@ -19,5 +34,33 @@ export const fetchIssuesService = async (
 ): Promise<any> => {
   const { issues } = octokit;
   const response = await issues.list(payload);
+  return response;
+};
+
+/**
+ * 获取 stars
+ * @param octokit 实例对象
+ * @param payload 参数
+ */
+export const fetchStarsService = async (
+  octokit: any,
+  payload = {},
+): Promise<any> => {
+  const { activity } = octokit;
+  const response = await activity.listReposStarredByAuthenticatedUser(payload);
+  return response;
+};
+
+/**
+ * 获取 public event
+ * @param octokit 实例对象
+ * @param payload 参数
+ */
+export const fetchPublicEventsService = async (
+  octokit: any,
+  payload = {},
+): Promise<any> => {
+  const { activity } = octokit;
+  const response = await activity.listPublicEvents(payload);
   return response;
 };
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,18 @@
 import Vue from 'vue';
 
+import {
+  YEAR_2019,
+} from '@/lib/constant';
+import {
+  fetchIssuesService,
+  fetchStarsService,
+  fetchUserInfoService,
+  fetchPublicEventsService,
+} from '@/api/service';
+
 const app = new Vue({
   data: {
+    userInfo: {},
     issues: [],
     code: '',
   },
@@ -25,6 +36,41 @@ export const updateState = (payload: ANY_OBJECT) => {
   Object.keys(payload).forEach(it => {
     st[it] = payload[it];
   });
+};
+
+export const fetchUserInfo = async (octokit: any) => {
+  const userInfo: any = [];
+  const res = await fetchUserInfoService(octokit);
+  st.userInfo = userInfo;
+};
+
+export const fetchIssues = async (octokit: any) => {
+  const issues: any = [];
+  const res = await fetchIssuesService(octokit, {
+    filter: 'all',
+    state: 'all',
+    sort: 'created',
+    since: YEAR_2019.toISOString(),
+    per_page: 100,
+    page: 1,
+  });
+  st.issues = issues;
+};
+
+export const fetchStars = async (octokit: any) => {
+  const stars: any = [];
+  const res = await fetchStarsService(octokit, {
+    sort: 'created',
+    per_page: 100,
+    page: 1,
+  });
+  st.stars = stars;
+};
+
+export const fetchPublicEvents = async (octokit: any) => {
+  const publicEvents: any = [];
+  const res = await fetchPublicEventsService(octokit);
+  st.publicEvents = publicEvents;
 };
 
 export default app;
